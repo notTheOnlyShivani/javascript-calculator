440af1ea304fbdcb36710117b79126c6ec06e1e5
Shivani Suresh Ghadge <shivanisureshghadge@Shivanis-MacBook-Air.local>
2023-01-19 01:20:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,62 +2,75 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [display, setDisplay] = useState("")
-  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const [output, setOutput] = useState("0")
+  const [input, setInput] = useState("0")
   const operators = ["AC", "/", "x", "+", "-", "="];
 
-const handleClick = (event) => {
-const number = event.target.textContent;
+const handleNumbers = (event) => {
+  const number = event.target.textContent;
+  if (!output.length) {
+    setInput(`${number}`);
+    setOutput(`${number}`);
+  } else {
+    if (number === "0" && (output === "0" || input === "0")) {
+      setOutput(`${output}`);
+    } else {
+      const lastChat = output.charAt(output.length - 1);
+      const isLastChatOperator =
+        lastChat === "*" || operators.includes(lastChat);
 
-if (display === '0') {
-  setDisplay(number);
-} else {
-  setDisplay(display + number);
+      setInput(isLastChatOperator ? `${number}` : `${input}${number}`);
+      setOutput(`${output}${number}`);
+    }
+  }
 }
-};
 
 const handleClear = () => {
-  setDisplay('0')
+  setOutput("")
+  setInput("0")
 }
 
 const handleSubmit = () => {
-  const total = eval(display);
-  setDisplay(total);
+  const total = eval(output);
+  setOutput(`${total}`);
+  setInput(total);
 }
 
 const dotOperator = () => {
-  const lastChat = display.charAt(display.length - 1);
-  if (!display.length) {
-    setDisplay(`0.`);
+  const lastChat = output.charAt(output.length - 1);
+  if (!output.length) {
+    setOutput("0.");
+    setInput("0.");
   } else { 
      if (lastChat === "*" || operators.includes(lastChat)) {
-    setDisplay(`${display}0.`)
+    setOutput(`${output} 0.`)
+    setInput("0.")
   } else {
-   if (numbers.includes(lastChat)) {
-    setDisplay(`${display}.`)
-  } else {
-    setDisplay(lastChat === "." ? `${display}` : `${display}.`);
-   }
-  }
+    setInput(
+      lastChat === "." || input.includes(".") ? `${input}` : `${input}.`
+    );
+    const formattedValue =
+    lastChat === "." || input.includes(".")
+      ? `${output}`
+      : `${output}.`;
+  setOutput(formattedValue);
+    }
   }
 }
-
-
-
-
 
 
 const handleOperator = (event) => {
   const operator = event.target.textContent;
-  if (display.length) {
-    setDisplay(`${display}${operator}`)
+  if (output.length) {
+    setOutput(`${output}${operator}`)
+    setInput(`${operator}`)
   }
-  const beforeLastChat = display.charAt(display.length - 2);
+  const beforeLastChat = output.charAt(output.length - 2);
 
   const beforeLastChatIsOperator =
     operators.includes(beforeLastChat) || beforeLastChat === "*";
 
-  const lastChat = display.charAt(display.length - 1);
+  const lastChat = output.charAt(output.length - 1);
   
   const lastChatIsOperator = operators.includes(lastChat) || lastChat === "*";
   
@@ -66,39 +79,40 @@ const handleOperator = (event) => {
     beforeLastChatIsOperator && lastChatIsOperator
   ) {
     if (beforeLastChatIsOperator) {
-      const updatedValue = `${display.substring(0, display.length - 2)}${operator}`;
-      setDisplay(updatedValue);
+      const updatedValue = `${output.substring(0, output.length - 2)}${operator}`;
+      setOutput(updatedValue);
   } else {
-    setDisplay(`${display.substring(0, display.length - 1)}${operator}`);
+    setOutput(`${output.substring(0, output.length - 1)}${operator}`);
   }
 } else {
-  setDisplay(`${display}${operator}`);
+  setOutput(`${output}${operator}`);
 }
 }
 
   return (
     <div className="App">
      <div className="calculator">
-      <div id="display">
-        {display}
-      </div>
+      {/* <div id="display"> */}
+      <div id="display" className="output">{output}</div>
+      <div id="display" className="input">{input}</div>
+        {/* </div> */}
       <div className="grid-container">
       <button className="buttons" id="clear" onClick={handleClear}>AC</button>
       <button className="buttons" id="divide" onClick={handleOperator}>/</button>
       <button className="buttons" id="multiply" onClick={handleOperator}>*</button>
-      <button className="buttons" id="seven" onClick={handleClick}>7</button>
-      <button className="buttons" id="eight" onClick={handleClick}>8</button>
-      <button className="buttons" id="nine" onClick={handleClick}>9</button>
+      <button className="buttons" id="seven" onClick={handleNumbers}>7</button>
+      <button className="buttons" id="eight" onClick={handleNumbers}>8</button>
+      <button className="buttons" id="nine" onClick={handleNumbers}>9</button>
       <button className="buttons" id="subtract" onClick={handleOperator}>-</button>
-      <button className="buttons" id="four" onClick={handleClick}>4</button>
-      <button className="buttons" id="five" onClick={handleClick}>5</button>
-      <button className="buttons" id="six" onClick={handleClick}>6</button>
+      <button className="buttons" id="four" onClick={handleNumbers}>4</button>
+      <button className="buttons" id="five" onClick={handleNumbers}>5</button>
+      <button className="buttons" id="six" onClick={handleNumbers}>6</button>
       <button className="buttons" id="add" onClick={handleOperator}>+</button>
-      <button className="buttons" id="one" onClick={handleClick}>1</button>
-      <button className="buttons" id="two" onClick={handleClick}>2</button>
-      <button className="buttons" id="three" onClick={handleClick}>3</button>
+      <button className="buttons" id="one" onClick={handleNumbers}>1</button>
+      <button className="buttons" id="two" onClick={handleNumbers}>2</button>
+      <button className="buttons" id="three" onClick={handleNumbers}>3</button>
       <button className="buttons" id="equals" onClick={handleSubmit}>=</button>
-      <button className="buttons" id="zero" onClick={handleClick}>0</button>
+      <button className="buttons" id="zero" onClick={handleNumbers}>0</button>
       <button className="buttons" id="decimal" onClick={dotOperator}>.</button>
       </div>
      </div>
